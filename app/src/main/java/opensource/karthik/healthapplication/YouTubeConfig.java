ced87d6d07482424956a671eb921819a48b357c9
Burhanuddin Rajbhoy <Burhan@vlan-2659-10-19-226-98.student.wireless.sydney.edu.au>
2017-10-24 23:48:54 +1100

    Added youtube videos to health infor centre

---
+++ app/src/main/java/opensource/karthik/healthapplication/YouTubeConfig.java
@@ -0,0 +1,17 @@
+package opensource.karthik.healthapplication;
+
+/**
+ * Created by Burhan on 10/24/17.
+ */
+
+public class YouTubeConfig {
+
+    public YouTubeConfig () {
+    }
+
+    private static final String API_KEY = "AIzaSyBf9gBbE71_147sImWT1g973bikUBqNMas";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
