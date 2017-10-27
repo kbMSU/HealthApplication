6ed266f1c1310907ac8d4f4fa4e1a4d9ad949443
Ahnaf Tabrez Alam <ahnaf.tabrez@gmail.com>
2017-10-27 12:17:49 +1100

    Stable layout design

--- app/src/main/java/opensource/karthik/healthapplication/HealthTracker.java
+++ app/src/main/java/opensource/karthik/healthapplication/HealthTracker.java
@@ -3,6 +3,14 @@ package opensource.karthik.healthapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class HealthTracker extends AppCompatActivity {
 
     @Override
